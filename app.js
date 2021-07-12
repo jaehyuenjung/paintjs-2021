0c4b21fe6213a886f111aef502aba4bf651c6640
JaeHyuen Jung <jaehyuen0613@gmail.com>
2021-07-12 13:37:31 +0900

    2D Context

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,23 +13,24 @@ function stopPainting() {
     painting = false;
 }
 
-function onMouseMove(event) {
-    const x = event.offsetX;
-    const y = event.offsetY;
-    console.log(x, y);
-}
-
-function onMouseDown(event) {
+function startPainting() {
     painting = true;
 }
 
-function onMouseUp(event) {
-    stopPainting();
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if (!painting) {
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", oneMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
