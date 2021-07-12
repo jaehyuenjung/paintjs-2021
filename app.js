a8cff317432607ff4c7a120f521824bb682c2342
JaeHyuen Jung <jaehyuen0613@gmail.com>
2021-07-12 13:50:30 +0900

    Changing Color

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const canvas = document.getElementById("jsCanvas");
+const colors = document.getElementsByClassName("jsColor");
 const ctx = canvas.getContext("2d");
 
 canvas.width = 700;
@@ -21,11 +22,17 @@ function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
     if (!painting) {
+        ctx.beginPath();
         ctx.moveTo(x, y);
     } else {
         ctx.lineTo(x, y);
         ctx.stroke();
     }
+}
+
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
 }
 
 if (canvas) {
@@ -34,3 +41,7 @@ if (canvas) {
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => {
+    color.addEventListener("click", handleColorClick);
+})
